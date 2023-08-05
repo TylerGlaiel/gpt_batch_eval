7fddb1fe61035de426d328eca095c4a2ffed0099
Tyler Glaiel <TylerGlaiel@users.noreply.github.com>
2023-08-05 13:44:16 -0700

    Update example.py

--- example.py
+++ example.py
@@ -13,10 +13,10 @@ batch1 = gpt_batch_eval("output the verb that describes how you use this item", 
 print(batch1.run())
 #prints [('knife', 'cut'), ('fork', 'eat'), ('gun', 'shoot'), ('sword', 'fight'), ('spoon', 'eat'), ('spork', 'eat'), ('spife', 'eat'), ('chopstick', 'eat'), ('throwing spoon', 'throw')]
 
-batch2 = gpt_batch_eval("classify as cutlery or weapon", data, validate=ValidResult)
+batch2 = gpt_batch_eval("classify as cutlery or weapon", data)
 print(batch2.run())
 #prints [('knife', 'cutlery'), ('fork', 'cutlery'), ('gun', 'weapon'), ('sword', 'weapon'), ('spoon', 'cutlery'), ('spork', 'cutlery'), ('spife', 'cutlery'), ('chopstick', 'cutlery'), ('throwing spoon', 'cutlery')]
 
-batch3 = gpt_batch_eval("translate into spanish", data, validate=ValidResult)
+batch3 = gpt_batch_eval("translate into spanish", data)
 print(batch3.run())
 #prints [('knife', 'cuchillo'), ('fork', 'tenedor'), ('gun', 'pistola'), ('sword', 'espada'), ('spoon', 'cuchara'), ('spork', 'cuchara tenedor'), ('spife', 'cuchillo y tenedor'), ('chopstick', 'palillo'), ('throwing spoon', 'cuchara de lanzamiento')]
